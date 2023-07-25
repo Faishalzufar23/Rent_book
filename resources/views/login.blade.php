f642383dbe14710e206822147e148f0cc735a74c
KnaF04 <alfankamil@gmail.com>
2023-07-25 09:37:39 +0700

    perubahan di login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Login Page</title>
+      <title>Login P</title>
 
 
       <!-- Bootstrap CSS -->
