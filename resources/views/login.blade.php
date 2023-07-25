28b715e66df5be8339fccda0bf284f30f97eeb5e
KnaF04 <alfankamil@gmail.com>
2023-07-25 16:04:09 +0700

    a

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Login Paaaaa</title>
+      <title>Login aaaaa</title>
 
 
       <!-- Bootstrap CSS -->
