f9263f060891e41d23134749e49f484b0af33a2c
KnaF04 <alfankamil@gmail.com>
2023-07-25 18:15:02 +0700

    terbaru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,19 +12,7 @@
 
 <body>
 <main class="flex-shrink-0">
-    <nav class="navbar navbar-expand-lg py-3" style="background-color: #FFC26F ">
-        <div class="container px-5" >
-            <a class="navbar-brand" href="#"><span class="fw-bolder">1001 LIBRARY</span></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder" style="color:#884A39">
-                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Catalog</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+@include('layout.nav')
 
 
     <header class="py-5">
@@ -78,22 +66,7 @@
         </div>
     </section>
 </main>
-
-
-<footer class="bg-white py-4 mt-auto">
-    <div class="container px-5">
-        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div class="col-auto"><div class="small m-0">Copyright &copy; Your Website 2023</div></div>
-            <div class="col-auto">
-                <a class="small" href="#!">Privacy</a>
-                <span class="mx-1">&middot;</span>
-                <a class="small" href="#!">Terms</a>
-                <span class="mx-1">&middot;</span>
-                <a class="small" href="#!">Contact</a>
-            </div>
-        </div>
-    </div>
-</footer>
+@include('layout.footer')
 
 
     @vite('resources/js/app.js')
